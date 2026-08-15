178fc2f2f71dd8c25908cfdf9af60c08152e89b9
Johannes Maron <johannes@maron.family>
2026-08-15 17:48:52 +0200

    Remove trailing prose from script output

--- bin/backup_new_key.sh
+++ bin/backup_new_key.sh
@@ -51,8 +51,3 @@ fi
 git add .box/backup.pub
 
 echo -e "${success_msg}SUCCESS${fin}"
-echo -e "${error}WARNING:${fin} Save .box/backup-private.key in a safe place."
-echo "You need it to decrypt and restore backups. It is not stored on GitHub."
-echo "To import on another machine: gpg --import <path-to>/backup-private.key"
-echo "Commit the public key yourself:"
-echo "  git commit -m 'Add backup encryption public key'"
